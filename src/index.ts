7ef6e43d22b09a2a8c9da8cebec9e95d58b0db5b
Martijn Dirksen <martijn.dirksen@protonmail.com>
2021-12-21 23:49:59 +0100

    wip

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// TODO write fetch api client code
+// TODO write neat wrapper for fetch
+// TODO investigate usage of cross-fetch
+// TODO read swagger spec
+// TODO Generate simple entities
+// TODO Generate disriminated entities
